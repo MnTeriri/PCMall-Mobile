52ddd51c0096f84750707bc3ade523dc47ceef21
Teriri <1511736399@qq.com>
2024-01-25 21:41:55 +0800

    登录页面Activity

--- app/src/main/java/com/example/pcmall/activity/LoginActivity.java
+++ app/src/main/java/com/example/pcmall/activity/LoginActivity.java
@@ -2,12 +2,17 @@ package com.example.pcmall.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.pcmall.databinding.ActivityLoginBinding;
+import com.example.pcmall.model.User;
+import com.example.pcmall.ui.viewmodel.LoginViewModel;
 import com.google.android.material.appbar.MaterialToolbar;
 
 import dagger.hilt.android.AndroidEntryPoint;
@@ -21,6 +26,18 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        LoginViewModel loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+
+
+        loginViewModel.getLoginUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                Log.d("LoginActivity", user.toString());
+            }
+        });
+
+        loginViewModel.login("000000000","12345");
 
         Button registerButton = binding.registerButton;
         registerButton.setOnClickListener(new View.OnClickListener() {
