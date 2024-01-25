16513c5941e70c941eddbdd1bcbd2bcd2ab8219c
Teriri <1511736399@qq.com>
2024-01-25 21:40:43 +0800

    结果返回状态枚举类

---
+++ app/src/main/java/com/example/pcmall/model/response/ResponseStatus.java
@@ -0,0 +1,21 @@
+package com.example.pcmall.model.response;
+
+import lombok.Getter;
+
+@Getter
+public enum ResponseStatus {
+    OK(200, "操作成功"),
+    NO_TOKEN_ERROR(500000, "没有token"),
+    TOKEN_EXPIRE_ERROR(500001, "token过期"),
+    AUTHORIZED_ERROR(401, "没有权限，需要登录"),
+    FORBIDDEN_ERROR(403, "权限不够，被拒绝");
+
+    private final Integer code;
+    private final String message;
+
+    ResponseStatus(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+}
