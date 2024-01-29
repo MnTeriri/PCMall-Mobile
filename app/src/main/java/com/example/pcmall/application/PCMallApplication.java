315c8fbfc23cf098775ee21e037057cdde7f38ee
Teriri <1511736399@qq.com>
2024-01-29 19:08:56 +0800

    PCMallApplication

--- app/src/main/java/com/example/pcmall/application/PCMallApplication.java
+++ app/src/main/java/com/example/pcmall/application/PCMallApplication.java
@@ -2,8 +2,15 @@ package com.example.pcmall.application;
 
 import android.app.Application;
 
+import com.example.pcmall.module.NetworkModule;
+
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
 public class PCMallApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NetworkModule.setOkHttpClientInstance(getApplicationContext());
+    }
 }
