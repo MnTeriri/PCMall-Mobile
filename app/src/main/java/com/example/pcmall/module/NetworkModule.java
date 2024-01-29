ad7bd2150f3b8aac1ea0eda8c120272dcb813bfc
Teriri <1511736399@qq.com>
2024-01-29 19:08:40 +0800

    Retrofit OKHttp Hint配置

--- app/src/main/java/com/example/pcmall/module/NetworkModule.java
+++ app/src/main/java/com/example/pcmall/module/NetworkModule.java
@@ -1,7 +1,13 @@
 package com.example.pcmall.module;
 
+import android.content.Context;
+
 import com.example.pcmall.service.LoginService;
 import com.example.pcmall.service.UserService;
+import com.franmontiel.persistentcookiejar.ClearableCookieJar;
+import com.franmontiel.persistentcookiejar.PersistentCookieJar;
+import com.franmontiel.persistentcookiejar.cache.SetCookieCache;
+import com.franmontiel.persistentcookiejar.persistence.SharedPrefsCookiePersistor;
 
 import javax.inject.Singleton;
 
@@ -9,6 +15,7 @@ import dagger.Module;
 import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.scalars.ScalarsConverterFactory;
@@ -17,12 +24,25 @@ import retrofit2.converter.scalars.ScalarsConverterFactory;
 @InstallIn(SingletonComponent.class)
 public class NetworkModule {
     public static String baseUrl = "http://192.168.31.109:10000/api/";
+    private static OkHttpClient okHttpClientInstance;
+
+    public static OkHttpClient setOkHttpClientInstance(Context context) {
+        if (okHttpClientInstance == null) {
+            ClearableCookieJar cookieJar =
+                    new PersistentCookieJar(new SetCookieCache(), new SharedPrefsCookiePersistor(context));
+            okHttpClientInstance = new OkHttpClient.Builder()
+                    .cookieJar(cookieJar)
+                    .build();
+        }
+        return okHttpClientInstance;
+    }
 
     @Singleton
     @Provides
     public static LoginService provideLoginService() {
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
+                .client(okHttpClientInstance)
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .build()
