3aff47d08a9270f3291ac1835a9505d0a558189d
Teriri <1511736399@qq.com>
2024-01-30 18:25:39 +0800

    RetrofitUtils

--- app/src/main/java/com/example/pcmall/utils/RetrofitUtils.java
+++ app/src/main/java/com/example/pcmall/utils/RetrofitUtils.java
@@ -1,16 +1,21 @@
 package com.example.pcmall.utils;
 
+import com.alibaba.fastjson2.JSON;
+import com.example.pcmall.model.response.ResponseResult;
+
 import java.io.IOException;
 
 import retrofit2.HttpException;
 
 public class RetrofitUtils {
-    public static String getErrorMessage(Throwable throwable) throws IOException {
-        String errorMessage = "";
+    public static <T> ResponseResult<T> getErrorMessage(Throwable throwable) throws IOException {
+        ResponseResult<T> responseResult = new ResponseResult<>();
         if (throwable instanceof HttpException) {
+            String errorMessage = "";
             HttpException httpException = (HttpException) throwable;
             errorMessage = httpException.response().errorBody().string();
+            responseResult = JSON.parseObject(errorMessage, ResponseResult.class);
         }
-        return errorMessage;
+        return responseResult;
     }
 }
