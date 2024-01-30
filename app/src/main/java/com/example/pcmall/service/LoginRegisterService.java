39b2ccd18404134421adc834e5c71a900125a421
Teriri <1511736399@qq.com>
2024-01-30 18:22:56 +0800

    LoginRegisterService

---
+++ app/src/main/java/com/example/pcmall/service/LoginRegisterService.java
@@ -0,0 +1,27 @@
+package com.example.pcmall.service;
+
+import com.example.pcmall.model.User;
+import com.example.pcmall.model.response.ResponseResult;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface LoginRegisterService {
+    @FormUrlEncoded
+    @POST("login")
+    Observable<ResponseResult<User>> login(@Field("uid") String uid,
+                                           @Field("password") String password,
+                                           @Field("code") String code);
+
+    @FormUrlEncoded
+    @POST("register")
+    Observable<ResponseResult<String>> register(@Field("uid") String uid,
+                                                @Field("password") String password,
+                                                @Field("code") String code);
+
+    @GET("captcha.jpg")
+    Observable<ResponseResult<String>> getCaptcha();
+}
