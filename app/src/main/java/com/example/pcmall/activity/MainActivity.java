300234e08c677b90595d4748068f9bb926c95f44
Teriri <1511736399@qq.com>
2024-01-30 18:23:45 +0800

    MainActivity

--- app/src/main/java/com/example/pcmall/activity/MainActivity.java
+++ app/src/main/java/com/example/pcmall/activity/MainActivity.java
@@ -1,13 +1,9 @@
 package com.example.pcmall.activity;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import com.example.pcmall.R;
-import com.example.pcmall.service.LoginService;
-import com.example.pcmall.service.UserService;
-import com.google.android.material.badge.BadgeDrawable;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.pcmall.service.LoginRegisterService;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -17,27 +13,15 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.pcmall.databinding.ActivityMainBinding;
 
-import java.io.IOException;
-
 import javax.inject.Inject;
 
 import dagger.hilt.android.AndroidEntryPoint;
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
-import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.core.Scheduler;
-import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Consumer;
-import io.reactivex.rxjava3.schedulers.Schedulers;
-import lombok.Data;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 @AndroidEntryPoint
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     @Inject
-    public LoginService loginService;
+    public LoginRegisterService loginRegisterService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
