87c2074bf6ef1b11c77cfd181605b4706586fdf9
Teriri <1511736399@qq.com>
2024-01-30 18:24:49 +0800

    PCMallApplication

--- app/src/main/java/com/example/pcmall/application/PCMallApplication.java
+++ app/src/main/java/com/example/pcmall/application/PCMallApplication.java
@@ -11,6 +11,6 @@ public class PCMallApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        NetworkModule.setOkHttpClientInstance(getApplicationContext());
+        NetworkModule.getOkHttpClientInstance(getApplicationContext());
     }
 }
