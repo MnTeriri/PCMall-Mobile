613d01cd24af777706fdcb26ddbdbb910f0d4616
Teriri <1511736399@qq.com>
2024-01-30 18:18:44 +0800

    FastJsonConverterFactory

---
+++ app/src/main/java/com/example/pcmall/converter/FastJsonConverterFactory.java
@@ -0,0 +1,39 @@
+package com.example.pcmall.converter;
+
+import androidx.annotation.Nullable;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
+
+public class FastJsonConverterFactory extends Converter.Factory {
+    private static ScalarsConverterFactory scalarsConverterFactory;
+
+    public static FastJsonConverterFactory create() {
+        scalarsConverterFactory = ScalarsConverterFactory.create();
+        return new FastJsonConverterFactory();
+    }
+
+    @Nullable
+    @Override
+    public Converter<?, RequestBody> requestBodyConverter(Type type, Annotation[] parameterAnnotations, Annotation[] methodAnnotations, Retrofit retrofit) {
+        return new FastJsonRequestBodyConverter<>();
+    }
+
+    @Nullable
+    @Override
+    public Converter<ResponseBody, ?> responseBodyConverter(Type type, Annotation[] annotations, Retrofit retrofit) {
+        Converter<ResponseBody, ?> responseBodyConverter = scalarsConverterFactory.responseBodyConverter(type, annotations, retrofit);
+        if (responseBodyConverter != null) {
+            return responseBodyConverter;
+        }
+        return new FastJsonResponseBodyConverter<>(type);
+    }
+
+
+}
