093ad8467cc37c8316bc7aa1e547d4aac5580ac9
Teriri <1511736399@qq.com>
2024-02-01 20:28:22 +0800

    HomeViewModel

--- app/src/main/java/com/example/pcmall/ui/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/example/pcmall/ui/viewmodel/HomeViewModel.java
@@ -1,19 +1,9 @@
 package com.example.pcmall.ui.viewmodel;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.pcmall.model.User;
-import com.example.pcmall.service.LoginRegisterService;
-
-import javax.inject.Inject;
-
-import dagger.hilt.android.lifecycle.HiltViewModel;
-
-@HiltViewModel
 public class HomeViewModel extends ViewModel {
     private final MutableLiveData<String> mText;
 
