9c42f4a0e254de9b8f8d25f193cb97db75ac5b72
Teriri <1511736399@qq.com>
2024-02-01 20:27:56 +0800

    PCMallApplication

--- app/src/main/java/com/example/pcmall/application/PCMallApplication.java
+++ app/src/main/java/com/example/pcmall/application/PCMallApplication.java
@@ -1,16 +1,38 @@
 package com.example.pcmall.application;
 
 import android.app.Application;
+import android.content.BroadcastReceiver;
+import android.content.SharedPreferences;
+import android.util.Log;
 
 import com.example.pcmall.module.NetworkModule;
+
+import javax.inject.Inject;
 
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
 public class PCMallApplication extends Application {
+    private final String TAG = "PCMallApplication";
+    @Inject
+    public SharedPreferences sharedPreferences;
+
     @Override
     public void onCreate() {
         super.onCreate();
-        NetworkModule.getOkHttpClientInstance(getApplicationContext());
+        //NetworkModule.getOkHttpClientInstance(getApplicationContext());
+        Log.d(TAG, "PCMallApplication启动");
+        boolean remember = sharedPreferences.getBoolean("remember", false);
+        if (!remember) {
+            Log.d(TAG, "用户没有记住账号，清空token和登录信息");
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("data", "");
+            editor.putString("token", "");
+            editor.apply();
+        }
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//        editor.putString("data", "");
+//        editor.putString("token", "");
+//        editor.apply();
     }
 }
