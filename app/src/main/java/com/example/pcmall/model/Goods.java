69d00cd5b9654652426fdd8262e4d01ef85040db
Teriri <1511736399@qq.com>
2024-02-22 21:24:36 +0800

    实体类

---
+++ app/src/main/java/com/example/pcmall/model/Goods.java
@@ -0,0 +1,29 @@
+package com.example.pcmall.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Accessors(chain = true)
+public class Goods {
+    private Integer id;//商品编号
+    private Integer cid;//分类编号，参考category的主键
+    private Integer bid;//品牌编号，参考brand的主键
+    private String gname;//商品名称
+    private LocalDateTime createdTime;//创建时间
+    private LocalDateTime updateTime;//修改时间
+    private String image;//图片
+    private BigDecimal price;//价格
+    private BigDecimal discount;//折扣
+    private Integer count;//数量
+    private String description;//商品描述
+    private Integer status;//商品状态（0正常、1缺货、2下架）
+    private Integer isDelete;//是否删除（0正常 1删除）
+}
--- app/src/main/java/com/example/pcmall/model/User.java
+++ app/src/main/java/com/example/pcmall/model/User.java
@@ -5,18 +5,19 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Accessors(chain = true)
 public class User {
     private Integer id;
     private String uid;//用户号
     private String uname;//用户名
     private String password;//密码（MD5加密）
-    private Integer isDelete;//0正常，1删除
     private LocalDateTime createdTime;//创建时间
     private LocalDateTime loginTime;//最后一次登录时间
-    //private byte[] image;
-    //private String imageString;//头像
+    private String image;
+    private Integer isDelete;//是否删除（0正常 1删除）
 }
