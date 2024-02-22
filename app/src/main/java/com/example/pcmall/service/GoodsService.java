2a2ad9613f95ddb7810df56234d958a79d80fd34
Teriri <1511736399@qq.com>
2024-02-22 21:25:18 +0800

    GoodsService

---
+++ app/src/main/java/com/example/pcmall/service/GoodsService.java
@@ -0,0 +1,19 @@
+package com.example.pcmall.service;
+
+import com.example.pcmall.model.Goods;
+import com.example.pcmall.model.response.ResponseResult;
+
+import java.util.List;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface GoodsService {
+    @FormUrlEncoded
+    @POST("searchGoodsList")
+    Observable<ResponseResult<List<Goods>>> searchGoodsList(
+            @Field("currentPage") Integer currentPage,
+            @Field("pageSize") Integer pageSize);
+}
