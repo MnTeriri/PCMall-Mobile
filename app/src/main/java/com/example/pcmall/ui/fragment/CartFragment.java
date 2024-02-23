89d0ec965b0a3cb67116c9810b67bd159e1333a5
Teriri <1511736399@qq.com>
2024-02-23 20:00:50 +0800

    CartFragment

--- app/src/main/java/com/example/pcmall/ui/fragment/CartFragment.java
+++ app/src/main/java/com/example/pcmall/ui/fragment/CartFragment.java
@@ -4,14 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.pcmall.databinding.FragmentCartBinding;
-import com.example.pcmall.ui.viewmodel.HomeViewModel;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
@@ -21,14 +18,9 @@ public class CartFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentCartBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
