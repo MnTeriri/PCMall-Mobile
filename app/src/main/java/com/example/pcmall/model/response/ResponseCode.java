939193c20efe51af9c68ea30b121f733c17e82ef
Teriri <1511736399@qq.com>
2024-03-21 15:38:17 +0800

    ResponseCode

--- app/src/main/java/com/example/pcmall/model/response/ResponseCode.java
+++ app/src/main/java/com/example/pcmall/model/response/ResponseCode.java
@@ -12,7 +12,10 @@ public enum ResponseCode {
     ACCOUNT_ERROR(500003, "账号或密码错误"),
     USER_EXIST_ERROR(500004, "账号存在"),
     ENTITY_NOT_FOUND(500005, "查询信息失败"),
-    STORAGE_NOT_ENOUGH_ERROR(500006, "商品库存不足"),
+    GOODS_NOT_ENOUGH_ERROR(500006, "商品缺货"),
+    GOODS_OFF_SHELF_ERROR(500007, "商品下架"),
+    CART_MIN_COUNT_ERROR(500008, "购物车数量最小"),
+    CART_GOODS_ERROR(500009, "购物车商品状态异常"),
     AUTHORIZED_ERROR(401, "没有权限，需要登录"),
     FORBIDDEN_ERROR(403, "权限不够，被拒绝"),
     INTERNAL_SERVER_ERROR(500, "服务器内部错误");
