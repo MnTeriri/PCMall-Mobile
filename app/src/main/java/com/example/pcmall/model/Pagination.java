114beee01c91eb960987375b915002ef572cdddc
Teriri <1511736399@qq.com>
2024-04-02 20:46:50 +0800

    Pagination

--- app/src/main/java/com/example/pcmall/model/Pagination.java
+++ app/src/main/java/com/example/pcmall/model/Pagination.java
@@ -16,7 +16,7 @@ public class Pagination {
     public Pagination() {
         this.currentPage = 1;
         this.totalPage = 1;
-        this.pageSize = 10;
+        this.pageSize = 20;
     }
 
     public Pagination(Integer pageSize) {
