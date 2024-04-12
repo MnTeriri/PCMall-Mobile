1103e94a29406c4aa16cf0df3266fc1d43295eff
Teriri <1511736399@qq.com>
2024-04-12 19:23:35 +0800

    AddressService

--- app/src/main/java/com/example/pcmall/service/AddressService.java
+++ app/src/main/java/com/example/pcmall/service/AddressService.java
@@ -6,6 +6,7 @@ import com.example.pcmall.model.response.ResponseResult;
 import java.util.List;
 
 import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
@@ -14,4 +15,10 @@ public interface AddressService {
     @FormUrlEncoded
     @POST("searchAddressList")
     Observable<ResponseResult<List<Address>>> searchAddressList(@Field("uid") String uid);
+
+    @POST("addAddress")
+    Observable<ResponseResult<String>> addAddress(@Body Address address);
+
+    @POST("updateAddress")
+    Observable<ResponseResult<String>> updateAddress(@Body Address address);
 }
