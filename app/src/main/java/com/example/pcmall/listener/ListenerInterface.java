44dcb42b90f790bf0297e5dc45986c3844db0093
Teriri <1511736399@qq.com>
2024-04-14 21:08:55 +0800

    ListenerInterface

---
+++ app/src/main/java/com/example/pcmall/listener/ListenerInterface.java
@@ -0,0 +1,35 @@
+package com.example.pcmall.listener;
+
+import android.view.View;
+
+import androidx.fragment.app.DialogFragment;
+
+public interface ListenerInterface {
+    public interface OnItemButtonClickListener<T> {
+        void onClick(View v, T data);
+    }
+
+    public interface OnItemCheckBoxClickListener<T> {
+        void onCheckedChanged(View v, T data);
+    }
+
+    public interface OnItemRadioButtonClickListener<T> {
+        void onClick(View v, T data);
+    }
+
+    public interface OnClickListener<T> {
+        void onClick(View v, T data);
+    }
+
+    public interface OnLongClickListener<T> {
+        boolean onLongClick(View v, T data);
+    }
+
+    public interface OnDialogClosedListener {
+        void onDialogClosed(DialogFragment dialogFragment);
+    }
+
+    public interface OnDialogClosedReturnDataListener<T> {
+        void onDialogClosed(DialogFragment dialogFragment, T data);
+    }
+}
