70925b0a4d2d0f515af493286271789f23943efb
Teriri <1511736399@qq.com>
2024-04-15 19:50:26 +0800

    实体类

--- app/src/main/java/com/example/pcmall/model/Order.java
+++ app/src/main/java/com/example/pcmall/model/Order.java
@@ -17,10 +17,10 @@ public class Order {
     private Integer id;
     private String oid;//订单编号
     private String uid;//用户编号
-    private Integer aid;//地址编号
     private List<Goods> goodsList;//订单商品信息
+    private Address address;//地址信息
     private BigDecimal price;//总金额
-    private Integer status;//状态（0待付款、1待发货、2待收货、3交易成功、4交易取消、5退货、6退货成功）
+    private Integer status;//状态（0待付款、1待发货、2待收货、3交易成功、4交易取消、5退货中、6退货成功）
     private LocalDateTime createdTime;//创建时间
     private LocalDateTime payTime;//付款时间
     private LocalDateTime sendTime;//发货时间
