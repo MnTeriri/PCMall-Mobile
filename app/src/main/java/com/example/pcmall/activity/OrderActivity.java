1ad6e6050b1faae192eb2227fe57c979c7a7e7b4
Teriri <1511736399@qq.com>
2024-04-15 21:42:11 +0800

    OrderActivity

--- app/src/main/java/com/example/pcmall/activity/OrderActivity.java
+++ app/src/main/java/com/example/pcmall/activity/OrderActivity.java
@@ -1,17 +1,25 @@
 package com.example.pcmall.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.example.pcmall.adapter.OrderPagerAdapter;
 import com.example.pcmall.databinding.ActivityOrderBinding;
+import com.example.pcmall.ui.fragment.OrderFragment;
 import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
 public class OrderActivity extends AppCompatActivity {
+    private final String TAG = "OrderActivity";
     private ActivityOrderBinding binding;
 
     @Override
@@ -20,11 +28,45 @@ public class OrderActivity extends AppCompatActivity {
         binding = ActivityOrderBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Intent intent = getIntent();
-        int tabId = intent.getIntExtra("tabId", 0);
+        initData();
+        initView();
+        initListener();
+        handelObserve();
 
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_ALL));
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_PAY));
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_SEND));
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_DELIVER));
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_REFUND));
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_COMMENT));
+
+        OrderPagerAdapter orderPagerAdapter = new OrderPagerAdapter(this, fragmentList);
+
+        //禁用预加载
+        binding.viewPager.setOffscreenPageLimit(ViewPager2.OFFSCREEN_PAGE_LIMIT_DEFAULT);
+        binding.viewPager.setAdapter(orderPagerAdapter);
+
+        new TabLayoutMediator(binding.orderTab, binding.viewPager, (tab, i) -> {
+            tab.setText(((OrderFragment) fragmentList.get(i)).getTitle());
+        }).attach();
+
+        int tabId = getIntent().getIntExtra("tabId", 0);
         TabLayout orderTab = binding.orderTab;
         TabLayout.Tab tabAt = orderTab.getTabAt(tabId);
         tabAt.select();
+    }
+
+    private void initListener() {
+    }
+
+    private void handelObserve() {
     }
 }
