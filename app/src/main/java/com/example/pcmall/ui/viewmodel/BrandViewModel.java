8434f311a5bc7b9346fd6c162d4970c96c129583
Teriri <1511736399@qq.com>
2024-04-17 16:51:02 +0800

    ViewModel

--- app/src/main/java/com/example/pcmall/ui/viewmodel/BrandViewModel.java
+++ app/src/main/java/com/example/pcmall/ui/viewmodel/BrandViewModel.java
@@ -5,11 +5,9 @@ import android.util.Log;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.example.pcmall.model.Cart;
-import com.example.pcmall.model.Category;
+import com.example.pcmall.model.Brand;
 import com.example.pcmall.model.response.ResponseCode;
-import com.example.pcmall.model.response.ResponseResult;
-import com.example.pcmall.service.CategoryService;
+import com.example.pcmall.service.BrandService;
 
 import java.util.List;
 
@@ -19,38 +17,37 @@ import dagger.hilt.android.lifecycle.HiltViewModel;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.Getter;
 
 @HiltViewModel
-public class CategoryViewModel extends ViewModel {
-    private final String TAG = "CategoryViewModel";
-    private final CategoryService categoryService;
+public class BrandViewModel extends ViewModel {
+    private final String TAG = "BrandViewModel";
+    private final BrandService brandService;
     private final CompositeDisposable compositeDisposable;
 
     @Getter
-    private final MutableLiveData<List<Category>> categoryListLiveData;//用户购物车信息
+    private final MutableLiveData<List<Brand>> brandListLiveData;//品牌信息
     @Getter
     private final MutableLiveData<Integer> flagLiveData;
 
     @Inject
-    public CategoryViewModel(CategoryService categoryService) {
-        this.categoryService = categoryService;
+    public BrandViewModel(BrandService brandService) {
+        this.brandService = brandService;
         this.compositeDisposable = new CompositeDisposable();
-        this.categoryListLiveData = new MutableLiveData<>();
+        this.brandListLiveData = new MutableLiveData<>();
         this.flagLiveData = new MutableLiveData<>();
         Log.d(TAG, "自动注入CategoryService完成");
         Log.d(TAG, "MutableLiveData初始化完成");
     }
 
-    public void getCategoryList() {
-        Disposable disposable = categoryService.searchCategoryList()
+    public void getBrandList(Integer cid) {
+        Disposable disposable = brandService.searchBrandByCid(cid)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(responseResult -> {
                     Log.d(TAG, responseResult.toString());
-                    categoryListLiveData.setValue(responseResult.getData());
+                    brandListLiveData.setValue(responseResult.getData());
                 }, throwable -> {
                     flagLiveData.setValue(ResponseCode.ERROR.getCode());
                 });
@@ -62,6 +59,6 @@ public class CategoryViewModel extends ViewModel {
         super.onCleared();
         compositeDisposable.clear();
         compositeDisposable.dispose();
-        Log.d(TAG, "销毁GoodsViewModel，清除compositeDisposable");
+        Log.d(TAG, "销毁BrandViewModel，清除compositeDisposable");
     }
 }
