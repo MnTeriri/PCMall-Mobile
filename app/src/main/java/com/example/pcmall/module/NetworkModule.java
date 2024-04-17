9a1379d40666f60acf091bcb1b789438c61dcd4d
Teriri <1511736399@qq.com>
2024-04-17 16:50:29 +0800

    NetworkModule

--- app/src/main/java/com/example/pcmall/module/NetworkModule.java
+++ app/src/main/java/com/example/pcmall/module/NetworkModule.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import com.example.pcmall.converter.FastJsonConverterFactory;
 import com.example.pcmall.interceptor.HeaderInterceptor;
 import com.example.pcmall.service.AddressService;
+import com.example.pcmall.service.BrandService;
 import com.example.pcmall.service.CartService;
 import com.example.pcmall.service.CategoryService;
 import com.example.pcmall.service.GoodsService;
@@ -84,6 +85,19 @@ public class NetworkModule {
                 .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
                 .build()
                 .create(CategoryService.class);
+    }
+
+    @Singleton
+    @Provides
+    public static BrandService provideBrandService(@ApplicationContext Context context) {
+        Log.d(TAG, "CategoryService生成");
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl + "mobile/brand/")
+                .client(getOkHttpClientInstance(context))
+                .addConverterFactory(FastJsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build()
+                .create(BrandService.class);
     }
 
     @Singleton
