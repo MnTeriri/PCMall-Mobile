aedcc304699c9a60bef3ba58e2bab9e713acad4f
Teriri <1511736399@qq.com>
2024-04-27 15:45:40 +0800

    Activity

--- app/src/main/java/com/example/pcmall/activity/OrderActivity.java
+++ app/src/main/java/com/example/pcmall/activity/OrderActivity.java
@@ -12,6 +12,8 @@ import com.example.pcmall.adapter.recyclerview.OrderListAdapter;
 import com.example.pcmall.adapter.viewpager2.OrderPagerAdapter;
 import com.example.pcmall.application.PCMallApplication;
 import com.example.pcmall.databinding.ActivityOrderBinding;
+import com.example.pcmall.dialog.MessageDialog;
+import com.example.pcmall.dialog.OrderDetailDialog;
 import com.example.pcmall.model.Order;
 import com.example.pcmall.model.Pagination;
 import com.example.pcmall.model.User;
@@ -26,6 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
 import dagger.hilt.android.AndroidEntryPoint;
 
 @AndroidEntryPoint
@@ -78,12 +81,12 @@ public class OrderActivity extends AppCompatActivity {
         fragmentList.add(new OrderFragment(OrderFragment.TITLE_SEND));
         fragmentList.add(new OrderFragment(OrderFragment.TITLE_DELIVER));
         fragmentList.add(new OrderFragment(OrderFragment.TITLE_REFUND));
-        fragmentList.add(new OrderFragment(OrderFragment.TITLE_COMMENT));
+        fragmentList.add(new OrderFragment(OrderFragment.TITLE_FINISH));
 
         OrderPagerAdapter orderPagerAdapter = new OrderPagerAdapter(this, fragmentList);
 
         //禁用预加载
-        binding.viewPager.setOffscreenPageLimit(6);
+
         binding.viewPager.setAdapter(orderPagerAdapter);
 
         new TabLayoutMediator(binding.orderTab, binding.viewPager, (tab, i) -> {
@@ -131,6 +134,73 @@ public class OrderActivity extends AppCompatActivity {
                 orderListAdapter.notifyDataSetChanged();
             }
         });
+
+        //点击订单Card进入订单详情界面
+        orderListAdapter.setOnClickListener((v, order) -> {
+            OrderDetailDialog dialog = new OrderDetailDialog(this, order);
+            dialog.setOnDialogClosedListener(dialogFragment -> {
+                Log.d(TAG, dialogFragment.getTag() + "关闭！");
+                orderViewModel.searchOrderList(searchValue, user.getUid(), -1, 1, pagination.getCurrentPage() * pagination.getPageSize(), true);
+                orderViewModel.getRecordsFiltered(searchValue, user.getUid(), -1);
+            });
+            dialog.show();
+        });
+
+        //点击付款按钮
+        orderListAdapter.setOnPayButtonClickListener((v, order) ->
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("付款界面")
+                        .setContentText("这是一个付款界面，如需付款点击付款按钮")
+                        .setConfirmText("付款")
+                        .setConfirmClickListener(dialog -> {
+                            orderViewModel.payOrder(order.getOid());
+                            dialog.dismissWithAnimation();
+                        })
+                        .setCancelText("取消")
+                        .setCancelClickListener(SweetAlertDialog::dismissWithAnimation)
+                        .show());
+
+        //点击确认签收按钮
+        orderListAdapter.setOnFinishButtonClickListener((v, order) ->
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("确认界面")
+                        .setContentText("确认签收？")
+                        .setConfirmText("确定")
+                        .setConfirmClickListener(dialog -> {
+                            orderViewModel.finishOrder(order.getOid());
+                            dialog.dismissWithAnimation();
+                        })
+                        .setCancelText("取消")
+                        .setCancelClickListener(SweetAlertDialog::dismissWithAnimation)
+                        .show());
+
+        //点击取消订单按钮
+        orderListAdapter.setOnCancelButtonClickListener((v, order) ->
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("取消订单界面")
+                        .setContentText("是否取消该订单")
+                        .setConfirmText("确认")
+                        .setConfirmClickListener(dialog -> {
+                            orderViewModel.cancelOrder(order.getOid());
+                            dialog.dismissWithAnimation();
+                        })
+                        .setCancelText("取消")
+                        .setCancelClickListener(SweetAlertDialog::dismissWithAnimation)
+                        .show());
+
+        //点击退货按钮
+        orderListAdapter.setOnRefundButtonClickListener((v, order) ->
+                new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("申请退货界面")
+                        .setContentText("是否申请退货")
+                        .setConfirmText("确认")
+                        .setConfirmClickListener(dialog -> {
+                            orderViewModel.refundOrder(order.getOid());
+                            dialog.dismissWithAnimation();
+                        })
+                        .setCancelText("取消")
+                        .setCancelClickListener(SweetAlertDialog::dismissWithAnimation)
+                        .show());
     }
 
     private void handelObserve() {
@@ -154,6 +224,34 @@ public class OrderActivity extends AppCompatActivity {
             } else if (Objects.equals(flag, OrderViewModel.LOAD_ERROR)) {
                 binding.refreshLayout.finishRefresh(false);//传入false表示刷新失败
                 binding.refreshLayout.finishLoadMore(false);
+            } else if (Objects.equals(flag, OrderViewModel.PAY_SUCCESS)) {
+                SweetAlertDialog sweetAlertDialog = new MessageDialog(this, SweetAlertDialog.SUCCESS_TYPE).setTitleText("订单付款成功！");
+                sweetAlertDialog.setOnDismissListener(dialog -> {
+                    orderViewModel.searchOrderList(searchValue, user.getUid(), -1, 1, pagination.getCurrentPage() * pagination.getPageSize(), true);
+                    orderViewModel.getRecordsFiltered(searchValue, user.getUid(), -1);
+                });
+                sweetAlertDialog.show();
+            } else if (Objects.equals(flag, OrderViewModel.FINISH_SUCCESS)) {
+                SweetAlertDialog sweetAlertDialog = new MessageDialog(this, SweetAlertDialog.SUCCESS_TYPE).setTitleText("订单签收成功！");
+                sweetAlertDialog.setOnDismissListener(dialog -> {
+                    orderViewModel.searchOrderList(searchValue, user.getUid(), -1, 1, pagination.getCurrentPage() * pagination.getPageSize(), true);
+                    orderViewModel.getRecordsFiltered(searchValue, user.getUid(), -1);
+                });
+                sweetAlertDialog.show();
+            } else if (Objects.equals(flag, OrderViewModel.CANCEL_SUCCESS)) {
+                SweetAlertDialog sweetAlertDialog = new MessageDialog(this, SweetAlertDialog.SUCCESS_TYPE).setTitleText("订单取消成功！");
+                sweetAlertDialog.setOnDismissListener(dialog -> {
+                    orderViewModel.searchOrderList(searchValue, user.getUid(), -1, 1, pagination.getCurrentPage() * pagination.getPageSize(), true);
+                    orderViewModel.getRecordsFiltered(searchValue, user.getUid(), -1);
+                });
+                sweetAlertDialog.show();
+            } else if (Objects.equals(flag, OrderViewModel.REFUND_SUCCESS)) {
+                SweetAlertDialog sweetAlertDialog = new MessageDialog(this, SweetAlertDialog.SUCCESS_TYPE).setTitleText("申请退货成功！");
+                sweetAlertDialog.setOnDismissListener(dialog -> {
+                    orderViewModel.searchOrderList(searchValue, user.getUid(), -1, 1, pagination.getCurrentPage() * pagination.getPageSize(), true);
+                    orderViewModel.getRecordsFiltered(searchValue, user.getUid(), -1);
+                });
+                sweetAlertDialog.show();
             } else if (Objects.equals(flag, ResponseCode.ERROR.getCode())) {
                 Toast.makeText(this, "错误！", Toast.LENGTH_SHORT).show();
             }
