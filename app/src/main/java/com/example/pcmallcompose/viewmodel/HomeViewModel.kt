54dbf3bd1e175792e88421420de12dbe245853c5
Teriri <1511736399@qq.com>
2024-07-25 18:23:42 +0800

    HomeViewModel

--- app/src/main/java/com/example/pcmallcompose/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/pcmallcompose/viewmodel/HomeViewModel.kt
@@ -3,13 +3,30 @@ package com.example.pcmallcompose.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
 import com.example.pcmallcompose.model.Goods
+import com.example.pcmallcompose.paging.GoodsPagingSource
+import com.example.pcmallcompose.service.GoodsService
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-
+    private val goodsService: GoodsService
 ) : ViewModel() {
     private val goodsList: LiveData<Goods> = MutableLiveData()
+    fun getGoodsPagingData(
+        searchValue: String = "",
+        pageSize: Int = 20,
+        initialLoadSize: Int = pageSize
+    ): Flow<PagingData<Goods>> {
+        return Pager(
+            PagingConfig(pageSize = pageSize, initialLoadSize = initialLoadSize)
+        ) {
+            GoodsPagingSource(goodsService, searchValue)
+        }.flow
+    }
 }
