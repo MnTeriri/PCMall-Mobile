c6b3dd0a292fa2d0b1c87a25c0e0a13b52bbdaca
Teriri <1511736399@qq.com>
2024-07-25 18:21:57 +0800

    Entity

--- app/src/main/java/com/example/pcmallcompose/model/Entity.kt
+++ app/src/main/java/com/example/pcmallcompose/model/Entity.kt
@@ -1,5 +1,11 @@
 package com.example.pcmallcompose.model
 
+import com.alibaba.fastjson2.annotation.JSONField
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import java.math.BigDecimal
 import java.time.LocalDateTime
 
@@ -20,12 +26,26 @@ data class Address(
 data class Brand(
     var id: Int? = null, //品牌id
     var bname: String? = null,//品牌名称
+    @field:JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @field:JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @field:JsonSerialize(using = LocalDateTimeSerializer::class)
     var createdTime: LocalDateTime? = null,//创建时间
+    @field:JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @field:JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @field:JsonSerialize(using = LocalDateTimeSerializer::class)
     var updateTime: LocalDateTime? = null,//修改时间
     var categoryCount: Long? = null,
     var image: String? = null,
     var isDelete: Int? = null, //是否删除（0正常 1删除）
-)
+){
+    fun setIsDelete(isDelete: Int?) {
+        this.isDelete = isDelete;
+    }
+
+    fun getIsDelete(): Int? {
+        return this.isDelete;
+    }
+}
 
 data class Cart(
     var id: Int? = null, //购物车信息编号
@@ -40,10 +60,24 @@ data class Cart(
 data class Category(
     var id: Int? = null, //分类编号
     var cname: String? = null, //分类名称
+    @field:JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @field:JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @field:JsonSerialize(using = LocalDateTimeSerializer::class)
     var createdTime: LocalDateTime? = null, //创建时间
+    @field:JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @field:JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @field:JsonSerialize(using = LocalDateTimeSerializer::class)
     var updateTime: LocalDateTime? = null,//修改时间
     var isDelete: Int? = null,//是否删除（0正常 1删除）
-)
+) {
+    fun setIsDelete(isDelete: Int?) {
+        this.isDelete = isDelete;
+    }
+
+    fun getIsDelete(): Int? {
+        return this.isDelete;
+    }
+}
 
 data class Goods(
     var id: Int? = null, //商品编号
@@ -52,7 +86,13 @@ data class Goods(
     var bid: Int? = null, //品牌编号，参考brand的主键
     var brand: Brand? = null,
     var gname: String? = null,//商品名称
+    @field:JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @field:JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @field:JsonSerialize(using = LocalDateTimeSerializer::class)
     var createdTime: LocalDateTime? = null,//创建时间
+    @field:JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @field:JsonDeserialize(using = LocalDateTimeDeserializer::class)
+    @field:JsonSerialize(using = LocalDateTimeSerializer::class)
     var updateTime: LocalDateTime? = null, //修改时间
     var image: String? = null, //图片
     var price: BigDecimal? = null, //价格
@@ -61,4 +101,12 @@ data class Goods(
     var description: String? = null,//商品描述
     var status: Int? = null,//商品状态（0正常、1缺货、2下架）
     var isDelete: Int? = null, //是否删除（0正常 1删除）
-)
+){
+    fun setIsDelete(isDelete: Int?) {
+        this.isDelete = isDelete;
+    }
+
+    fun getIsDelete(): Int? {
+        return this.isDelete;
+    }
+}
