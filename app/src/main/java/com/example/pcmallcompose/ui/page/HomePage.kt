004a7c6a2739660a90b850b7e42301f5a64dc40a
Teriri <1511736399@qq.com>
2024-08-01 18:20:43 +0800

    界面

--- app/src/main/java/com/example/pcmallcompose/ui/page/HomePage.kt
+++ app/src/main/java/com/example/pcmallcompose/ui/page/HomePage.kt
@@ -1,38 +1,71 @@
 package com.example.pcmallcompose.ui.page
 
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.pullrefresh.PullRefreshIndicator
-import androidx.compose.material.pullrefresh.pullRefresh
-import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
+import androidx.compose.material3.pulltorefresh.PullToRefreshBox
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.traversalIndex
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.pcmallcompose.R
 import com.example.pcmallcompose.model.Goods
 import com.example.pcmallcompose.ui.theme.PCMallComposeTheme
 import com.example.pcmallcompose.viewmodel.HomeViewModel
@@ -41,61 +74,96 @@ import kotlinx.coroutines.launch
 
 const val TAG = "HomePage"
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomePage(viewModel: HomeViewModel = hiltViewModel()) {
-    var presses by remember { mutableIntStateOf(0) }
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                colors = topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                ),
-                title = {
-                    Text("HomePage")
-                }
-            )
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            GoodsListView(viewModel.getGoodsPagingData().collectAsLazyPagingItems())
-        }
-
+fun HomePage(homeViewModel: HomeViewModel = hiltViewModel()) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .semantics { isTraversalGroup = true }
+    ) {
+        SearchBarView(modifier = Modifier.align(Alignment.TopCenter))
+        GoodsListView(
+            modifier = Modifier.padding(top = 104.dp),
+            lazyPagingItems = homeViewModel.getGoodsPagingData().collectAsLazyPagingItems()
+        )
     }
 }
 
-@OptIn(ExperimentalMaterialApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GoodsListView(lazyPagingItems: LazyPagingItems<Goods>) {
+fun SearchBarView(modifier: Modifier) {
+    var text by rememberSaveable { mutableStateOf("") }
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    SearchBar(
+        modifier = modifier.semantics { traversalIndex = 0f },
+        inputField = {
+            SearchBarDefaults.InputField(
+                query = text,
+                onQueryChange = { text = it },
+                onSearch = { expanded = false },
+                expanded = expanded,
+                onExpandedChange = { expanded = it },
+                placeholder = { Text("搜索商品") },
+                leadingIcon = { Icon(Icons.Default.Search, contentDescription = null) },
+                trailingIcon = { Icon(Icons.Default.MoreVert, contentDescription = null) },
+            )
+        },
+        expanded = expanded,
+        onExpandedChange = { expanded = it },
+    ) {
+        Column(Modifier.verticalScroll(rememberScrollState())) {
+            repeat(4) { idx ->
+                val resultText = "Suggestion $idx"
+                ListItem(
+                    headlineContent = { Text(resultText) },
+                    supportingContent = { Text("Additional info") },
+                    leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
+                    colors = ListItemDefaults.colors(containerColor = Color.Transparent),
+                    modifier =
+                    Modifier
+                        .clickable {
+                            text = resultText
+                            expanded = false
+                        }
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 4.dp)
+                )
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun GoodsListView(modifier: Modifier, lazyPagingItems: LazyPagingItems<Goods>) {
     val coroutineScope = rememberCoroutineScope()
     //是否在刷新
     var isRefreshing by remember { mutableStateOf(false) }
     //下拉刷新的状态管理
-    val pullRefreshState = rememberPullRefreshState(
-        refreshing = isRefreshing,
-        onRefresh = {//刷新时所做动作
-            coroutineScope.launch {
-                isRefreshing = true//设置正在刷新
-                delay(1000)
-                lazyPagingItems.refresh()
-                isRefreshing = false //刷新业务执行完毕后修改状态
-            }
+    val state = rememberPullToRefreshState()
+
+    val onRefresh: () -> Unit = {
+        coroutineScope.launch {
+            isRefreshing = true//设置正在刷新
+            delay(1000)
+            lazyPagingItems.refresh()
+            isRefreshing = false //刷新业务执行完毕后修改状态
         }
-    )
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pullRefresh(state = pullRefreshState)
+    }
+
+    PullToRefreshBox(
+        modifier = modifier,
+        state = state,
+        isRefreshing = isRefreshing,
+        onRefresh = onRefresh,
     ) {
-        LazyColumn {
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .fillMaxSize()
+                .semantics { isTraversalGroup = true }
+        ) {
             items(
                 count = lazyPagingItems.itemCount,
                 key = { index ->
@@ -110,25 +178,41 @@ fun GoodsListView(lazyPagingItems: LazyPagingItems<Goods>) {
                 }
             }
         }
-        PullRefreshIndicator(
-            refreshing = isRefreshing,
-            state = pullRefreshState,
-            modifier = Modifier.align(Alignment.TopCenter)
-        )
     }
 }
 
+@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun GoodsItemView(goods: Goods) {
-    Text(
-        modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 20.dp),
-        text = "${goods.id},${goods.gname}"
-    )
+    Log.d(TAG, goods.toString())
+    ElevatedCard(
+        modifier = Modifier.padding(8.dp)
+    ) {
+        Column {
+            Image(painter = painterResource(R.drawable.img), contentDescription = "")
+
+            Text(
+                modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 20.dp),
+                text = "${goods.brand?.bname} ${goods.gname}"
+            )
+            Text(
+                modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 20.dp),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = "${goods.description}"
+            )
+            Text(
+                modifier = Modifier.padding(20.dp, 20.dp, 20.dp, 20.dp),
+                color = colorResource(R.color.danger),
+                text = "￥${goods.price}"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingHomePagePagePreview() {
+fun GreetingHomePagePreview() {
     PCMallComposeTheme {
         HomePage()
     }
