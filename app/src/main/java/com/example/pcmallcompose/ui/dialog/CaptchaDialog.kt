7324c696c4e4e9163a9e9242b9b5984af5a88b01
Teriri <1511736399@qq.com>
2024-09-18 14:15:24 +0800

    CaptchaDialog

---
+++ app/src/main/java/com/example/pcmallcompose/ui/dialog/CaptchaDialog.kt
@@ -0,0 +1,173 @@
+package com.example.pcmallcompose.ui.dialog
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusState
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pcmallcompose.R
+import com.example.pcmallcompose.ui.page.LoginPage
+import com.example.pcmallcompose.ui.theme.PCMallComposeTheme
+import com.example.pcmallcompose.utils.ImageUtils
+
+@Composable
+fun CaptchaDialog(
+    onDismissRequest: () -> Unit,
+    onConfirmation: (captcha: String) -> Unit,
+    onClickCaptchaImage: () -> Unit,
+    onReloadCaptchaImage: () -> Unit,
+    captchaImage: String,
+    isError: Boolean = false,
+    errorMessage: String = "",
+    validate: (String) -> Unit = {},
+) {
+    var text by remember { mutableStateOf("") }
+
+    AlertDialog(
+        title = {
+            Text(text = "请输入验证码")
+        },
+        text = {
+            Column {
+                Text(text = "请在下方输入框输入图片验证码")
+                if (captchaImage == "") {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .padding(top = 5.dp)
+                            .align(Alignment.CenterHorizontally),
+                        color = MaterialTheme.colorScheme.secondary,
+                        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+                    )
+                } else {
+                    Image(
+                        modifier = Modifier
+                            .size(width = 135.dp, height = 50.dp)
+                            .padding(top = 5.dp)
+                            .align(Alignment.CenterHorizontally)
+                            .clickable {
+                                onClickCaptchaImage()
+                            },
+                        bitmap = ImageUtils.decodeImageString(captchaImage),
+                        contentDescription = null,
+                    )
+                    TextButton(
+                        modifier = Modifier
+                            .size(width = Dp.Unspecified, height = 25.dp)
+                            .padding(top = 5.dp)
+                            .align(Alignment.CenterHorizontally),
+                        contentPadding = PaddingValues(0.dp),
+                        shape = RoundedCornerShape(0),
+                        onClick = {
+                            onReloadCaptchaImage()
+                        }
+                    ) {
+                        Text(
+                            fontSize = 12.sp,
+                            color = Color.Black,
+                            text = "看不清？换一张",
+                        )
+                    }
+                }
+
+                OutlinedTextField(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 5.dp),
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text(text = "验证码") },
+                    supportingText = {
+                        if (isError) {
+                            Text(errorMessage)
+                        }
+                    },
+                    isError = isError,
+                    singleLine = true
+                )
+            }
+        },
+        onDismissRequest = {
+            onDismissRequest()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    validate(text)
+                    onConfirmation(text)
+                }
+            ) {
+                Text("确定")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismissRequest()
+                }
+            ) {
+                Text("取消")
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CaptchaDialogPreview() {
+    PCMallComposeTheme {
+        var errorMessage by rememberSaveable { mutableStateOf("") }
+        var isError by rememberSaveable { mutableStateOf(false) }
+
+        CaptchaDialog(
+            onDismissRequest = { /*TODO*/ },
+            onConfirmation = { /*TODO*/ },
+            onClickCaptchaImage = { /*TODO*/ },
+            onReloadCaptchaImage = {/*TODO*/ },
+            captchaImage = "",
+            isError = isError,
+            errorMessage = errorMessage,
+            validate = {
+                isError = it.length != 5
+                if (isError) {
+                    errorMessage = "aaaaaaa"
+                }
+            }
+        )
+    }
+}
