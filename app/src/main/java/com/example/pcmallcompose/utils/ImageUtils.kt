6a3001b1ba0d5b100b59934004b1c984f6d3a324
Teriri <1511736399@qq.com>
2024-09-18 14:15:09 +0800

    ImageUtils

---
+++ app/src/main/java/com/example/pcmallcompose/utils/ImageUtils.kt
@@ -0,0 +1,18 @@
+package com.example.pcmallcompose.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+
+import java.util.Base64;
+
+class ImageUtils {
+    companion object {
+        @JvmStatic
+        fun decodeImageString(imageString: String): ImageBitmap {
+            val bytes = Base64.getMimeDecoder().decode(imageString)
+            return BitmapFactory.decodeByteArray(bytes, 0, bytes.size).asImageBitmap()
+        }
+    }
+}
